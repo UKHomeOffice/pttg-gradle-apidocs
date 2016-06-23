b7940f433a6a4db0a665bc17b45a11a03898ba93
atodd <alistair.todd@digital.homeoffice.gov.uk>
2016-06-23 09:54:29 +0100

    Documentation

--- src/main/groovy/uk/gov/digital/ho/proving/apidocsgradleplugin/UsageTask.groovy
+++ src/main/groovy/uk/gov/digital/ho/proving/apidocsgradleplugin/UsageTask.groovy
@@ -13,44 +13,14 @@ class UsageTask extends DefaultTask {
 
     @TaskAction
     def usageReport() {
-        println "\n" +
-            "************************************ USAGE ********************************************\n" +
-            "pttgApiDocsGradle plugin usage guide\n" +
-            "\n" +
-             "This plugin applies the following configuration\n" +
-            "\n" +
-            "Plugins:\n" +
-            "    Asciidoctor\n" +
-            "\n" +
-            "Repositories:\n" +
-            "    maven - https://repo.spring.io/libs-milestone:\n" +
-            "\n" +
-            "Dependencies:\n" +
-            "    spring-restdocs-core\n" +
-            "    spring-restdocs-restassured\n" +
-            "\n" +
-            "Source Sets:\n" +
-            "   doc\n" +
-            "\n" +
-            "The following tasks are added\n" +
-            "\n" +
-            "    apiDocTest : execute the api documentation tests\n" +
-            "\n" +
-            "    generateApiDocs: execute the documentation tests and generate the final HTML and PDF documentation\n" +
-            "\n" +
-            "    buildWithApiDocs: perform a full build and add the generated documentation to the final jar\n" +
-            "\n" +
-            "The plugin expects a conventional layout of source adoc files and tests\n" +
-            "The standard layout can be modified using any of the following properties\n" +
-            "\n" +
-            "    apidocs{\n" +
-            "        testSrcPattern = 'apidocs/**'\n" +
-            "        sourceDocsDir = 'src/doc'\n" +
-            "        jarDocsDir = 'static/docs'\n" +
-            "        snippetsDir = 'build/generated-snippets'\n" +
-            "    }\n" +
-            "\n" +
-            "See full details in the plugin readme in github at https://github.com/UKHomeOffice/pttg-gradle-apidocs"
+
+        try {
+            def usage = this.getClass().getResource('/usage/usage.txt').text
+            println usage
+        } catch (Exception e) {
+            println "Error loading usage report"
+            e.printStackTrace()
+        }
 
     }
 }
